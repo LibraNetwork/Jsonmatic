0ad1d53340f3223ad688317dda5214646249b53b
ByNamles <50907368+ByNamles@users.noreply.github.com>
2023-08-10 00:53:39 +0300

    fix

--- src/Jsonmatic/util/JsonmaticUtil.php
+++ src/Jsonmatic/util/JsonmaticUtil.php
@@ -17,7 +17,7 @@ class JsonmaticUtil
         $yCoordinates = array_map(fn ($coordinate) => explode(":", $coordinate)[1], array_keys($jsonmatic));
         $zCoordinates = array_map(fn ($coordinate) => explode(":", $coordinate)[2], array_keys($jsonmatic));
         $firstPosition = new Vector3(max($xCoordinates), max($yCoordinates), min($zCoordinates));
-        $secondPosition = new Vector3(min($xCoordinates["x"]), min($yCoordinates["y"]), max($zCoordinates["z"]));
+        $secondPosition = new Vector3(min($xCoordinates), min($yCoordinates), max($zCoordinates));
         return new Vector3(($firstPosition->getX() + $secondPosition->getX()) / 2, max($firstPosition->getY(), $secondPosition->getY()), ($firstPosition->getZ() + $secondPosition->getZ()) / 2);
     }
 }
